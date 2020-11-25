b2689940a68cb1e29cdbb13c43a355da8fce8e07
Robin Paul <robinpaul288@gmail.com>
2020-11-26 01:02:08 +0530

    _

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express();
 
 const data = require('./data.json')
+const config = require('./config.json')
+const jwt = require('jsonwebtoken');
+
 let middlewareObj = require('./middleware');
 let bodyParser     = require('body-parser');
 let cors = require('cors')
@@ -31,16 +34,22 @@ app.get('/api/members/:name', (req,res)=>{
 })
 
 app.post('/api/members/',middlewareObj.checkdataisvalid,(req,res) => {
-    
-    
     let d = req.body
-  
-
-    
-
     console.log(d)
     data.push(d)
 	res.status('200').send('Successfully added');
+})
+
+app.post('/api/login/', (req,res) => {
+  
+   if(req.body.email != null && req.body.password != null){
+        const token = jwt.sign({ sub: '7' }, config.secret, { expiresIn: '7d' });
+        res.cookie("SESSIONID", token, {httpOnly:false, secure:true}).json({
+            message: 'User found',
+            token:token
+          });
+
+  }
 })
 
 const port = process.env.PORT || 3000;
